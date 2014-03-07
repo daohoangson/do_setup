26582f88fd82ec9bec75d5e379e062db0dd4fb08
Đào Hoàng Sơn <daohoangson@gmail.com>
2014-03-07 18:27:16 +0700

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -9,20 +9,19 @@ yum install logwatch -y
 yum install postfix -y
 read -p "Opening logwatch config now. Prepare to change MailTo and MailFrom..."
 vi /usr/share/logwatch/default.conf/logwatch.conf
-echo "Running logwatch..."
+echo "Running logwatch. You should receive an email like 'Logwatch for $HOSTNAME'..."
 /usr/sbin/logwatch
-read -p "Opening crontab now. Prepare to add '6 29 * * * /usr/sbin/logwatch'..."
+read -p "Opening crontab now. Prepare to add '29 6 * * * /usr/sbin/logwatch'..."
 crontab -e
 
 rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 yum install fail2ban -y
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
-read -p "Opening fail2ban config now. You may want to disable ssh iptables"
+read -p "Opening fail2ban config now. You may want to disable ssh-iptables"
 vi /etc/fail2ban/jail.local
 service fail2ban restart
 
 useradd $USER
-read -p "Prepare to enter password for $USER..."
 passwd $USER
 mkdir /home/$USER
 mkdir /home/$USER/.ssh
@@ -31,9 +30,10 @@ read -p "Openning authorized_keys now, ready to paste your .pub content"
 vi /home/$USER/.ssh/authorized_keys
 chmod 400 /home/$USER/.ssh/authorized_keys
 chown $USER:$USER /home/$USER -R
-read -p "Insert `$USER ALL=(ALL) ALL` to grant sudo"
+read -p "Insert '$USER ALL=(ALL) ALL' to grant sudo"
 visudo
 
-read -p "Opening sshd config now, you should: 1, change port; 2, PasswordAuthentication no + PubkeyAuthentication yes; 3, PermitRootLogin no"
+read -p "Opening sshd config now, you should: 1, change port; 2, PasswordAuthentication no; 3, PermitRootLogin no"
 vi /etc/ssh/sshd_config
 service sshd restart
+echo "Restarted sshd, please try ssh into the server with $USER account before ending this SSH session"
